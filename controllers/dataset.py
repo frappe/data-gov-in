a9ec04f90549ad90855aa5e99dbd6a9cc461fa4b
Rushabh Mehta <rmehta@gmail.com>
2013-04-12 12:01:00 +0530

    [fixes] updated graphs, fixed crash issues and added link to group

--- controllers/dataset.py
+++ controllers/dataset.py
@@ -1,5 +1,6 @@
 import json
 import utils, properties
+import re
 consolelog = []
 
 def get_args(form_dict):
@@ -13,38 +14,48 @@ def get_args(form_dict):
 		"properties": file_properties,
 		"group": group,
 		"group_info": properties.groups[group],
-		"consolelog": consolelog
+		"consolelog": consolelog,
+		"json": json,
+		"len": len
 	}
 	
 def get_chart_data(file_data, file_properties):
-	global consolelog
-	chart_type = file_properties.get("chart_type")
-	if not chart_type:
-		return
-	
-	data_index = file_properties["data_index"]
-	x_labels = file_data[file_properties["x_axis"]][data_index:]
-	map_dataset = file_data
-	if file_properties["transpose"]:
+	# global consolelog
+	# chart_type = file_properties.get("chart_type")
+	# if not chart_type:
+	# 	return
+	chart_type="Line"
+
+	if False: #transpose
 		map_dataset = map(list, zip(*file_data))
-		x_labels = map_dataset[file_properties["x_axis"]][data_index:]
+	else:
+		map_dataset = [[val for val in row] for row in file_data]
+
+	map_dataset = exclude_total_type_columns(map_dataset, 1)
+	map_dataset, start_row, start_column = get_numeric_dataset(map_dataset)
+
+	x_labels = file_data[0][1:]
+	x_labels = [label[:15] for label in file_data[0][start_column:]]
+	x_label_color = []
 	
 	data_sets = []
 	color_steps = int(255.0 / (len(map_dataset) - 1))
 	i = 0
-	for d in map_dataset[data_index:]:
+	for row in map_dataset:
 		i += color_steps
 		if chart_type == "Line":
-			data_sets.append({
+			data_set_row = {
 				"fillColor" : "rgba(%i, %i, %i, %s)" % (i, i/1.2, i/4, .3),
 				"strokeColor" : "rgba(%i, %i, %i, %s)" % (i, i/1.2, i/4, 1),
 				"pointColor" : "rgba(%i, %i, %i, %s)" % (i, i, i/1.2, 1),
 				"pointStrokeColor" : "#fff",
-				"data": [utils.flt(val) for val in d[data_index:]]
-			})
+				"data": [utils.flt(val) for val in row]
+			}
+			data_sets.append(data_set_row)
+			x_label_color.append(data_set_row["strokeColor"])
 		elif chart_type == "Pie":
 			data_sets.append({
-				"value": utils.flt(d[data_index]),
+				"value": utils.flt(row[1]),
 				"color": "rgba(%i, %i, %i, %s)" % (i, i/1.2, i/4, .3),
 			})
 	
@@ -54,4 +65,49 @@ def get_chart_data(file_data, file_properties):
 		"datasets": data_sets
 	}
 		
-	return json.dumps(chart_data)
+	return chart_data
+
+def exclude_total_type_columns(map_dataset, start_row):
+	global consolelog
+	
+	start_row_data = map_dataset[start_row-1]
+	original_col_len = len(start_row_data)
+	excluded = [cidx for cidx in xrange(original_col_len) \
+		if "total" in start_row_data[cidx].lower()]
+	new_dataset = []
+	for row in map_dataset:
+		new_row = []
+		new_dataset.append(new_row)
+		for cidx in xrange(original_col_len):
+			if not cidx in excluded:
+				new_row.append(row[cidx])
+		consolelog.append(new_row)
+				
+	return new_dataset
+		
+
+def get_numeric_dataset(file_data):
+	map_dataset = file_data
+	start_column = 1
+	start_row = 1
+	
+	def has_numeric_value(row):
+		ret = False
+		for value in row:
+			if re.search('[0-9.]+', value):
+				ret = True
+				break
+		return ret
+		
+	while not has_numeric_value(map_dataset[start_row]) and start_row < len(map_dataset):
+		start_row += 1
+	
+	while re.search('[a-zA-Z]+', map_dataset[start_row][start_column]) and start_column < len(map_dataset[start_row]):
+		start_column += 1
+		if start_column > 20:
+			break
+			
+	map_dataset = [row[start_column:] for row in map_dataset[start_row:]]
+	
+	return map_dataset, start_row, start_column
+	
