18330b5a1a7a247d383a6dad82cfc25203cda3a8
Anand Doshi <anand@erpnext.com>
2013-04-21 01:00:46 +0530

    [index.py] get json response when format=json is passed as a cgi field

--- index.py
+++ index.py
@@ -18,9 +18,14 @@ def render():
 	template = form_dict.get("page", "home")
 	args = get_method("controllers." + template + ".get_args")(form_dict)
 	args.update(form_dict)
-	html = jenv.get_template(template + ".html").render(args)
+	
+	if form_dict.get("format") == "json":
+		import json
+		html = json.dumps(args, default=json_handler)
+	else:
+		html = jenv.get_template(template + ".html").render(args)
 
-	print "Content-Type: text/html"
+	print "Content-Type: text/html; charset: utf-8"
 	print
 	print (html or "").encode("utf-8")
 
@@ -46,6 +51,20 @@ def set_jenv_filters(jenv):
 	"""import these python functions as jinja filters"""
 	from utils import urlencode
 	jenv.filters["urlencode"] = urlencode
+	
+def json_handler(obj):
+	"""serialize non-serializable data for json"""
+	import datetime
+	from types import ModuleType
+	
+	# serialize date
+	if isinstance(obj, (datetime.date, datetime.timedelta, datetime.datetime)):
+		return unicode(obj)
+	elif hasattr(obj, "__call__") or isinstance(obj, ModuleType):
+		return obj.__name__
+	else:
+		raise TypeError, """Object of type %s with value of %s is not JSON serializable""" % \
+			(type(obj), repr(obj))
 	
 if __name__=="__main__":
 	render()
